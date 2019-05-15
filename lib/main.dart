d1d11a927e6c706f9c15d53c36f03bf2f01eff4a
Amit Wani <mtwn105@gmail.com>
2019-05-15 15:42:57 +0530

    Basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Currency Xchange',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0xff3c00ca),
+        primaryColorDark: Color(0xff000098),
       ),
       home: HomePage(),
     );
@@ -27,31 +28,201 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController fromCurrencyController = new TextEditingController(text: "0.00");
+  TextEditingController fromCurrencyController =
+  new TextEditingController(text: "0.00");
   TextEditingController toCurrencyController = new TextEditingController();
-  double rate = 0.00;
+  double fromValue = 0;
+  String fromValueString = "0";
+  double rate = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery
+        .of(context)
+        .size;
+    var height = size.height;
+    var width = size.width;
+
+    TextStyle numberPadTextStyle = new TextStyle(
+        fontSize: 42.0, color: Colors.white, fontWeight: FontWeight.w100);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Currency Xchange"),
+      appBar: AppBar(backgroundColor: Theme
+          .of(context)
+          .primaryColorDark,
+        title: Text("Currency Xchange".toUpperCase(),
+          style: TextStyle(letterSpacing: 2.0),),
+        centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              keyboardType: TextInputType.number,
-              controller: fromCurrencyController,
-            ),
-            Text("Rs. ${rate*double.parse(fromCurrencyController.text)}"),
-            FlatButton(
-              onPressed: getData,
-              child: Text("Convert"),
-              color: Colors.blue,
-            )
-          ],
+      body: Container(
+        color: Theme
+            .of(context)
+            .primaryColor,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("USD"),
+                  ),
+                  Text("${fromValue.toString()}",
+                      style: TextStyle(fontSize: 72.0)),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("INR"),
+                  ),
+                  Text("${(rate * fromValue).toStringAsFixed(2)}",
+                      style: TextStyle(fontSize: 72.0)),
+                ],
+              ),
+              Expanded(
+                child: Container(),
+              ),
+              Container(
+                color: Theme
+                    .of(context)
+                    .primaryColorDark,
+                height: height * 0.4,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        MaterialButton(
+                            onPressed: () {
+                              numberTap("7");
+                            },
+                            child: Text(
+                              "7",
+                              style: numberPadTextStyle,
+                            )),
+                        MaterialButton(
+                            onPressed: () {
+                              numberTap("8");
+                            },
+                            child: Text(
+                              "8",
+                              style: numberPadTextStyle,
+                            )),
+                        MaterialButton(
+                            onPressed: () {
+                              numberTap("9");
+                            },
+                            child: Text(
+                              "9",
+                              style: numberPadTextStyle,
+                            )),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        MaterialButton(
+                            onPressed: () {
+                              numberTap("4");
+                            },
+                            child: Text(
+                              "4",
+                              style: numberPadTextStyle,
+                            )),
+                        MaterialButton(
+                            onPressed: () {
+                              numberTap("5");
+                            },
+                            child: Text(
+                              "5",
+                              style: numberPadTextStyle,
+                            )),
+                        MaterialButton(
+                            onPressed: () {
+                              numberTap("6");
+                            },
+                            child: Text(
+                              "6",
+                              style: numberPadTextStyle,
+                            )),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        MaterialButton(
+                            onPressed: () {
+                              numberTap("1");
+                            },
+                            child: Text(
+                              "1",
+                              style: numberPadTextStyle,
+                            )),
+                        MaterialButton(
+                            onPressed: () {
+                              numberTap("2");
+                            },
+                            child: Text(
+                              "2",
+                              style: numberPadTextStyle,
+                            )),
+                        MaterialButton(
+                            onPressed: () {
+                              numberTap("3");
+                            },
+                            child: Text(
+                              "3",
+                              style: numberPadTextStyle,
+                            )),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        MaterialButton(
+                            onPressed: () {
+                              numberTap(".");
+                            },
+                            child: Text(
+                              ".",
+                              style: numberPadTextStyle,
+                            )),
+                        MaterialButton(
+                            onPressed: () {
+                              numberTap("0");
+                            },
+                            child: Text(
+                              "0",
+                              style: numberPadTextStyle,
+                            )),
+                        MaterialButton(
+                            onPressed: () {
+                              backspaceTap();
+                            },
+                            child: Icon(
+                              Icons.backspace,
+                            )),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -64,10 +235,47 @@ class _HomePageState extends State<HomePage> {
     var data = json.decode(response.body);
     CurrencyModel currencyModel = CurrencyModel.fromJson(data);
     Currency currency = currencyModel.rates;
-    double base = currency.eUR.ceilToDouble();
     setState(() {
       rate = currency.iNR / currency.uSD;
     });
+
     print(rate);
+  }
+
+  void numberTap(String numberString) {
+    double from = double.parse(fromValueString);
+    if (from == 0) {
+      double numberTapped = double.parse(numberString);
+      from += numberTapped;
+      fromValueString = numberString;
+    } else if (numberString == ".") {
+      if (fromValueString.contains(".")) {} else {
+        fromValueString += ".";
+      }
+    } else {
+      fromValueString += numberString;
+    }
+    setState(() {
+      fromValue = (fromValueString.endsWith(".")
+          ? double.parse(fromValueString + "0")
+          : double.parse(fromValueString));
+    });
+
+    print(fromValueString);
+  }
+
+  void backspaceTap() {
+    int len = fromValueString.length;
+    if (len > 1) {
+      fromValueString = fromValueString.substring(0, len - 1);
+    } else {
+      fromValueString = "0";
+    }
+    setState(() {
+      fromValue = (fromValueString.endsWith(".")
+          ? double.parse(fromValueString + "0")
+          : double.parse(fromValueString));
+    });
+    print(fromValueString);
   }
 }
